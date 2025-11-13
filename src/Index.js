526d0b9315ac7d15378adc2b96b8d5de182a4c9a
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2025-11-13 01:40:50 +0100

    maxRatings

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={7} />
+    <StarRating maxRating={10} />
+
+{/* set a default value for the props in case no value is passed from the parent component ( Destructuring the props directly in the function parameter whenhere no value is passed from Index.js file) */}
+    <StarRating />
   </React.StrictMode>
 );
--- src/StarRating.js
+++ src/StarRating.js
@@ -1,14 +1,39 @@
 // You can take out the style object which does'nt change on anything that is inside the StarRating component so the style object does not regenerate by js each time the component re-renders
 //  otherwise each time the components re-renders the style object will be created again and again which is not optimal for performance. So we can take it out of the component so that it is created only once.
 
-// Create an empty array with length of 5 element which takes 2 argument _ means current element which we are not using and i means index of the current element
+const containerStyle = {
+  display: "flex",
+  gap: "16px",
+  alignItems: "center",
+};
 
-export default function StarRating () {
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textstyle = {
+  fontSize: "18px",
+  fontWeight: "bold",
+  lineHeight: "1",
+  margin: "0",
+};
+
+// set a default value for the props in case no value is passed from the parent component ( Destructuring the props directly in the function parameter whenhere no value is passed from Index.js file)
+export default function StarRating ({ maxRating = 5 }) {
   return (
-    <div style={{ display: "flex", gap: "4px" }}>
-      {Array.from({ length: 5 }, (_, i) => i).map((star) => (
-        <div key={star} style={{ width: "24px", height: "24px", backgroundColor: "gold" }} />
+  <div style={containerStyle}>
+    <div style={starContainerStyle}>
+
+{/* Create an empty array with length of 5 element which takes 2 argument _ means current element which we are not using and i means index of the current element */}
+      {Array.from({ length: maxRating }, (_, i) => (
+        <span >
+          S{i + 1}
+        </span>
       ))}
     </div>
-  )
+    <p style={textstyle}>10</p>
+  </div>
+)
+  
 }
