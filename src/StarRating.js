7ef3ae9b857726f1abf51b17743645ad4a6689d0
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2025-11-23 16:08:00 +0100

    default prop

--- src/StarRating.js
+++ src/StarRating.js
@@ -16,23 +16,29 @@ const starContainerStyle = {
   display: "flex",
 };
 
-// Add prop and give a default value in case no value is passed from parent component
-
-const textstyle = {
-  fontSize: "18px",
-  fontWeight: "bold",
-  lineHeight: "1",
-  margin: "0",
-};
-
 // set a default value for the props in case no value is passed from the parent component ( Destructuring the props directly in the function parameter whenhere no value is passed from Index.js file)
-export default function StarRating ({ maxRating = 5 }) {
+export default function StarRating ({ 
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  }) {
+
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
   }
+
+  // Add prop and give a default value in case no value is passed from parent component
+  const textstyle = {
+  fontWeight: "bold",
+  lineHeight: "1",
+  margin: "0",
+  color,
+  fontSize: `${size / 1.5}px`,
+};
 
   // Set a default className for any user in the future that wants to customize the star rating component(font style, font size, font-family, color etc)
   return (
@@ -46,7 +52,9 @@ export default function StarRating ({ maxRating = 5 }) {
        full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
         onRateClick={() => handleRating(i + 1)} 
         onHoverIn={() => setTempRating(i + 1)}
-        onHoverOut={() => setTempRating(0)}     
+        onHoverOut={() => setTempRating(0)} 
+        colorProp={color}
+        sizeProp={size}
        />
       ))}
     </div>
@@ -56,29 +64,29 @@ export default function StarRating ({ maxRating = 5 }) {
   );
 }
 
-  const starStyle = {
-  width: "48px",
-  height: "48px",
-  display: "block",
-  cursor: "pointer",
-}
-
-
 // role="button" for accessibility to let screen readers know that this is a button. use onclick event to handle click on Rating star
-function Star({ onRateClick, full, onHoverIn, onHoverOut }) {
+function Star({ onRateClick, full, onHoverIn, 
+  onHoverOut, colorProp, sizeProp }) {
+
+  const starStyle = {
+    width: `${sizeProp}px`,
+    height: `${sizeProp}px`,
+    display: "block",
+    cursor: "pointer",
+  };
 
   return (
-    <span role="button" style={starStyle} 
-    onClick={onRateClick} 
-    onMouseEnter={onHoverIn}
-     onMouseLeave={onHoverOut}>
+  <span role="button" style={starStyle} 
+  onClick={onRateClick} 
+  onMouseEnter={onHoverIn}
+    onMouseLeave={onHoverOut}>
 
 {/* Ternary operator to render full star or empty star based on the full prop passed from StarRating component  */}
       { full ? <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
-        fill="#000"
-        stroke="#000"
+        fill={colorProp}
+        stroke={colorProp}
       >
         <path
           d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -88,7 +96,7 @@ function Star({ onRateClick, full, onHoverIn, onHoverOut }) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke="#000"  
+      stroke={colorProp}
     >
   <path
     strokeLinecap="round"
