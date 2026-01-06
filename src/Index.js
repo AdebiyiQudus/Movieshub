900c86bb961c1b2284115f97a4e00de87b9709b0
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2026-01-06 01:30:12 +0100

    Bugs fixing

--- src/Index.js
+++ src/Index.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
-// Adding customization feature to the StarRating component so that other users can reuse the component according to (rating, different spacing in btw the stars and also asked the customer to spcify  text labels appears )
-
+import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
+
 
 // User Customization Example for reusing the Star
 
@@ -12,6 +11,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  <StarRating maxRating={5}></StarRating>
   </React.StrictMode>
 );
