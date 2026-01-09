2eeb22cf8b4e1d5d9569865b3d9162daa0dbad6a
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2026-01-09 01:15:37 +0100

    customize delete

--- src/Index.js
+++ src/Index.js
@@ -4,9 +4,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-
-// User Customization Example for reusing the Star
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
