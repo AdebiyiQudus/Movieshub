1732f04b4815fc5cdf4ca52e3e17cd5ce7a1aade
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2026-03-11 00:12:30 +0100

    watchedProp Movie

--- src/App.js
+++ src/App.js
@@ -344,6 +344,7 @@ function Movie({ movieProp, onSelectMovie }) {
 // ============== STATEFUL COMPONENT ===============
 function  MovieDetails({ selectedIdProp, 
   onCloseMovieE, onAddWatchedE, watchedProp }) { 
+    
   const [movie, setMovie] = useState({})
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
