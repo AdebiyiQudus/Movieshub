bf76be079b4df746faacfe89892d755c855d9a02
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2026-03-16 22:39:15 +0100

    Average Function

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ const tempWatchedData = [
 
 // PROP DRILLING => passing data from parent component to child component via props (Parsing prop from nested components to access data where needed i.e deeply nested components)
 
-// Qudus Syntax
+// AVERAGE FUNCTION => calculate the average of an array of numbers by summing all the elements and dividing by the length of the array, with a check to return 0 if the array is empty to avoid division by zero errors
 const average = (arr) =>
   arr.length === 0 ? 0
     : arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
