1d841703a349cda843326f353acc81a4ac9f18c8
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2026-04-08 01:02:21 +0100

    currency from

--- src/Currency Converter.js
+++ src/Currency Converter.js
@@ -1,14 +1,29 @@
 export default function App() {
+
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [exchangeRate, setExchangeRate] = useState(0);
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input 
+        type="text" 
+        value={amount} 
+        onChange={(e) => setAmount(Number(e.target.value))} 
+      />
+      <select 
+        value={fromCurrency} 
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select 
+        value={toCurrency} 
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
