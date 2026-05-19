771675d687723abd300bfabeb99a6d4e280e6edf
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2026-05-19 04:23:41 +0100

    useMovies Hook

---
+++ src/useMovies.js
@@ -0,0 +1,6 @@
+import { useState } from "react";
+
+// Find reason why my query doesn't clear/close input search when i click escape 
+export function useMovies  (query) {
+  const [movies, setMovies] = useState
+}
