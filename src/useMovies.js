78b33d5c647856eba27cdce3ee2a1b0a3b77330e
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2026-05-21 02:47:29 +0100

    import useCallback

--- src/useMovies.js
+++ src/useMovies.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { useCallback } from "react";
+
 
 const KEY = "45d089db"; // OMDB API key
 export function useMovies  (query, callback) {
