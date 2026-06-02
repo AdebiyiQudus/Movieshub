a1be356f31eba31618bea231457c457af2f5c7f7
Inquisitive <111200753+AdebiyiQudus@users.noreply.github.com>
2026-06-02 22:25:39 +0100

    Geolocation delete

--- src/useGeoLocation..js
+++ src/useGeoLocation..js
@@ -1,26 +1,26 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-export function useGeoLocation() {
-  const [location, setLocation] = useState({});
-  const [error, setError] = useState("");
+// export function useGeoLocation() {
+//   const [location, setLocation] = useState({});
+//   const [error, setError] = useState("");
 
-  useEffect(function() {
-    if (!navigator.geolocation) {
-      setError("Geolocation is not supported by your browser");
-      return;
-    }
+//   useEffect(function() {
+//     if (!navigator.geolocation) {
+//       setError("Geolocation is not supported by your browser");
+//       return;
+//     }
 
-    function success(position) {
-      setLocation({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      });
-    }
+//     function success(position) {
+//       setLocation({
+//         latitude: position.coords.latitude,
+//         longitude: position.coords.longitude,
+//       });
+//     }
 
-    function error(err) {
-      setError(err.message);
-    }
-    navigator.geolocation.getCurrentPosition(success, error);
-  }, []);
-  return { location, error };
-}
+//     function error(err) {
+//       setError(err.message);
+//     }
+//     navigator.geolocation.getCurrentPosition(success, error);
+//   }, []);
+//   return { location, error };
+// }
